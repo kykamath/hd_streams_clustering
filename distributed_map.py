b7a89c8ca80e26b05f82fcd3c6283aa045c0fdda
kykamath <krishna.kamath@gmail.com>
2011-06-12 23:15:09 -0500

    minor modifications.

--- distributed_map.py
+++ distributed_map.py
@@ -4,12 +4,22 @@ Created on Jun 12, 2011
 @author: kykamath
 '''
 
-class DistributedMongoMap:
+class DistributedMapException(Exception):
+    def __init__(self, error='Distributed map exception'):
+        self.message = error
+
+class DistributedMemcacheMap:
     '''
-    A distributed hashmap that uses mongodb.
+    A distributed hashmap that uses memcached.
     '''
-    def add(self):
-        print 15
+    try:
+        import memcache
+    except ImportError: print 'Install python-memcache to use DistributedMemcachedMap'
+    global memcache
+    def __init__(self, server, port=11211):
+        self.cache = memcache.Client(['%s:%s'%(server, port)], debug=0)
+        if self.cache.set('server_running', 1)==0: 
+            raise DistributedMapException('Memcached server not running at %s:%s'%(server, port))
 
 if __name__ == '__main__':
-    DistributedMongoMap().add()
+    DistributedMemcacheMap('localhost')
