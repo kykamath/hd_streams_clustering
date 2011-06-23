1d1350ad460c9c8594a2118a0baa82143a2a8b5a
Krishna Kamath <krishna.kamath@gmail.com>
2011-06-22 19:43:45 -0500

    modified classes

--- classes.py
+++ classes.py
@@ -8,25 +8,35 @@ from streaming_lsh.classes import Document
 from library.nlp import getPhrases, getWordsFromRawEnglishMessage
 from collections import defaultdict
 from library.vector import Vector
+from library.math_modified import exponentialDecay, DateTimeAirthematic
 
 class VectorUpdateMethods:
     @staticmethod
-    def addWithoutDecay(stream, vector): 
+    def addWithoutDecay(stream, vector, **kwargs): 
         for k,v in vector.iteritems():
             if k in stream: stream[k]+=v
             else: stream[k]=v
+    @staticmethod
+    def exponentialDecay(stream, vector, decayCoefficient, timeDifference):
+        for k in stream: stream[k]=exponentialDecay(stream[k], decayCoefficient, timeDifference)
+        for k in vector: 
+            if k in stream: stream[k]+=vector[k]
+            else: stream[k]=vector[k]
         
 class Stream(Document):
-    def __init__(self, streamId, vector): super(Stream, self).__init__(streamId, vector)
-    def updateWithVector(self, vector, updateMethod): 
-        self=updateMethod(self,vector)
-
+    def __init__(self, streamId, message): 
+        super(Stream, self).__init__(streamId, message.vector)
+        self.lastMessageTime = message.timeStamp
+    def updateForMessage(self, message, updateMethod, decayCoefficient, timeUnitInSeconds): 
+        timeDifference = None
+        if timeUnitInSeconds!=None: timeDifference = DateTimeAirthematic.getDifferenceInTimeUnits(message.timeStamp, self.lastMessageTime, timeUnitInSeconds)
+        updateMethod(self, message.vector, decayCoefficient=decayCoefficient, timeDifference=timeDifference)
 
 class Message:
-    def __init__(self, streamId, messageId, text, timestamp): 
-        self.streamId, self.messageId, self.text, self.timestamp = streamId, messageId, text, timestamp
-    def getVector(self, wordToIdMap, min_phrase_length, max_phrase_length):
-        vectorMap = defaultdict(float)
-        for phrase in getPhrases(getWordsFromRawEnglishMessage(self.text), min_phrase_length, max_phrase_length): vectorMap[wordToIdMap[phrase]]+=1
-        self.vector = Vector(vectorMap)
-        return self.vector
+    def __init__(self, streamId, messageId, text, timeStamp): 
+        self.streamId, self.messageId, self.text, self.timeStamp = streamId, messageId, text, timeStamp
+    def setVector(self, wordToIdMap=None, min_phrase_length=None, max_phrase_length=None):
+        if 'vector' not in self.__dict__: 
+            vectorMap = defaultdict(float)
+            for phrase in getPhrases(getWordsFromRawEnglishMessage(self.text), min_phrase_length, max_phrase_length): vectorMap[wordToIdMap[phrase]]+=1
+            self.vector = Vector(vectorMap)
--- tests/classes_tests.py
+++ tests/classes_tests.py
@@ -9,30 +9,44 @@ import unittest
 from library.nlp import StopWords
 from library.vector import Vector
 from classes import Stream, Message, VectorUpdateMethods
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class StreamTests(unittest.TestCase):
     def setUp(self):
-        self.stream = Stream(1, {1:1.,2:3.})
-    def test_updateWithVector(self):
-        self.stream.updateWithVector(Vector({2:3}), VectorUpdateMethods.addWithoutDecay)
+        self.now = datetime.now()
+        self.m1 = Message(1, 'sdf', 'A project to cluster high-dimensional streams.', self.now-timedelta(seconds=60))
+        self.m1.vector=Vector({1:1.,2:3.})
+        self.stream = Stream(1, self.m1)
+        self.m2 = Message(1, 'sdf', 'A project to cluster high-dimensional streams.', self.now)
+        self.m2.vector=Vector({2:3.})
+    def test_updateForMessage_addWithoutDecay(self):
+        self.stream.updateForMessage(self.m2, VectorUpdateMethods.addWithoutDecay, None, None)
         self.assertEqual(self.stream, Vector({1:1.,2:6.}))
-
+    def test_updateForMessage_exponentialDecay(self):
+        self.stream.updateForMessage(self.m2, VectorUpdateMethods.exponentialDecay, 0.5, 60)
+        self.assertEqual(self.stream, Vector({1:0.5,2:4.5}))
+        
 class MessageTests(unittest.TestCase):
     def setUp(self):
         self.message = Message(1, 'sdf', 'A project to cluster high-dimensional streams.', datetime.now())
         StopWords.load()
     def test_getVector(self):
         wordToIdMap = {'project':1, 'cluster': 2, 'highdimensional': 3, 'streams': 4}
-        self.assertEqual(Vector({1:1, 2:1, 3:1, 4:1}), self.message.getVector(wordToIdMap, 1, 1))
+        self.message.setVector(wordToIdMap, 1, 1)
+        self.assertEqual(Vector({1:1, 2:1, 3:1, 4:1}), self.message.vector)
         
 class VectorUpdateMethodTests(unittest.TestCase):
     def setUp(self): 
-        self.s1 = Stream(1, {1:2., 2:3.})
+        self.message = Message(1, 'sdf', 'A project to cluster high-dimensional streams.', datetime.now())
+        self.message.vector = Vector({1:2., 2:3.})
+        self.s1 = Stream(1, self.message)
         self.v1 = Vector({1:2., 3:3.})
     def test_addWithoutDecay(self):
         VectorUpdateMethods.addWithoutDecay(self.s1, self.v1)
         self.assertEqual(Vector({1: 4, 2: 3, 3: 3}), self.s1)
+    def test_exponentialDecay(self):
+        VectorUpdateMethods.exponentialDecay(self.s1, self.v1, 0.5, 1)
+        self.assertEqual(Vector({1: 3, 2: 1.5, 3: 3}), self.s1)
         
 if __name__ == '__main__':
     unittest.main()
