92bc8c7105b5c8ffcbd6ded4e44f067e192f26cb
Krishna Kamath <krishna.kamath@gmail.com>
2011-06-23 18:42:22 -0500

    updated twitter crowds

--- tests/twitter_crowds_tests.py
+++ tests/twitter_crowds_tests.py
@@ -6,16 +6,22 @@ Created on Jun 23, 2011
 import unittest
 from twitter_crowds import TwitterCrowdsSpecificMethods
 from library.vector import Vector
+from settings import twitter_stream_settings
+from classes import Phrase
+from datetime import datetime
+
+test_time = datetime.now()
 
 class TwitterCrowdsSpecificMethodsTests(unittest.TestCase):
     def setUp(self):
         self.text = 'A project to cluster high-dimensional streams.'
         self.min_phrase_length, self.max_phrase_length, self.max_dimensions = 1, 1, 4
         self.phraseTextToIdMap = {'project':0, 'cluster': 1}
+        self.phraseTextToPhraseObjectMap = {'project': Phrase('project', test_time, score=8), 'cluster': Phrase('cluster', test_time, score=8)}
         self.tweet = {'user':{'screen_name': 'abc'}, 'id':10, 'text':self.text, 'created_at': 'Tue Mar 01 05:59:59 +0000 2011'}
         self.vector = Vector({0:1, 1:1, 2:1, 3:1})
     def test_getMessageObjectForTweet(self):
-        message = TwitterCrowdsSpecificMethods.getMessageObjectForTweet(self.tweet, self.phraseTextToIdMap, self.max_dimensions, self.min_phrase_length, self.max_phrase_length)
+        message = TwitterCrowdsSpecificMethods.getMessageObjectForTweet(self.tweet, self.phraseTextToIdMap, self.phraseTextToPhraseObjectMap, **twitter_stream_settings)
         self.assertEqual(self.vector, message.vector)
         
 if __name__ == '__main__':
--- twitter_crowds.py
+++ twitter_crowds.py
@@ -10,23 +10,19 @@ import pprint
 
 class TwitterCrowdsSpecificMethods:
     @staticmethod
-    def getMessageObjectForTweet(tweet, phraseToIdMap, max_dimensions, min_phrase_length, max_phrase_length):
-        message = Message(tweet['user']['screen_name'], tweet['id'], tweet['text'], getDateTimeObjectFromTweetTimestamp(tweet['created_at']))
-        message.vector = UtilityMethods.getVectorForText(tweet['text'], phraseToIdMap, max_dimensions, min_phrase_length, max_phrase_length)
+    def getMessageObjectForTweet(tweet, phraseTextToIdMap, phraseTextToPhraseObjectMap, **twitter_stream_settings):
+        tweetTime = getDateTimeObjectFromTweetTimestamp(tweet['created_at'])
+        message = Message(tweet['user']['screen_name'], tweet['id'], tweet['text'], tweetTime)
+        message.vector = UtilityMethods.getVectorForText(tweet['text'], tweetTime, phraseTextToIdMap, phraseTextToPhraseObjectMap, **twitter_stream_settings)
         return message
 
-phraseToIdMap = {}
+phraseTextToIdMap, phraseTextToPhraseObjectMap = {}, {}
 
 def tweetsFromFile():
     for tweet in TweetFiles.iterateTweetsFromGzip('data/sample.gz'):
-        print TwitterCrowdsSpecificMethods.getMessageObjectForTweet(tweet, phraseTextToIdMap=phraseTextToIdMap, 
-                                                                    max_dimensions=twitter_stream_settings.max_dimensions,
-                                                                    min_phrase_length=twitter_stream_settings.min_phrase_length,
-                                                                    max_phrase_length=twitter_stream_settings.max_phrase_length
-                                                                    ).vector
-    print len(phraseToIdMap)
-    print pprint.pprint(phraseToIdMap)
+        print TwitterCrowdsSpecificMethods.getMessageObjectForTweet(tweet, phraseTextToIdMap, phraseTextToPhraseObjectMap, **twitter_stream_settings).vector
+    print len(phraseTextToIdMap)
+    print pprint.pprint(phraseTextToIdMap)
     
 if __name__ == '__main__':
     tweetsFromFile()
-    ph = Phrase('asd', d, score)
