db19a1a131f9233d1b30d13482df0d7d5f73d53e
Krishna Kamath <krishna.kamath@gmail.com>
2011-06-24 00:02:17 -0500

    updated twitter crowds

--- twitter_crowds.py
+++ twitter_crowds.py
@@ -5,8 +5,8 @@ Created on Jun 22, 2011
 '''
 from settings import twitter_stream_settings
 from library.twitter import TweetFiles, getDateTimeObjectFromTweetTimestamp
+from library.classes import GeneralMethods
 from classes import Message, UtilityMethods, Stream, VectorUpdateMethods
-import pprint
 from datetime import timedelta
 
 phraseTextToIdMap, phraseTextToPhraseObjectMap, streamIdToStreamObjectMap = {}, {}, {}
@@ -19,15 +19,18 @@ class TwitterCrowdsSpecificMethods:
         message = Message(tweet['user']['screen_name'], tweet['id'], tweet['text'], tweetTime)
         message.vector = UtilityMethods.getVectorForText(tweet['text'], tweetTime, phraseTextToIdMap, phraseTextToPhraseObjectMap, **twitter_stream_settings)
         return message
+    @staticmethod
+    def printParam(param): print param
 
 def tweetsFromFile():
-    
-    for tweet in TweetFiles.iterateTweetsFromGzip('data/sample.gz'):
+#    for tweet in TweetFiles.iterateTweetsFromGzip('data/sample.gz'):
+    for tweet in TweetFiles.iterateTweetsFromGzip('/mnt/chevron/kykamath/data/twitter/filter/2011_2_6.gz'):
         message = TwitterCrowdsSpecificMethods.getMessageObjectForTweet(tweet, phraseTextToIdMap, phraseTextToPhraseObjectMap, **twitter_stream_settings)
         if message.streamId not in streamIdToStreamObjectMap: streamIdToStreamObjectMap[message.streamId] = Stream(message.streamId, message)
         else: streamIdToStreamObjectMap[message.streamId].updateForMessage(message, VectorUpdateMethods.exponentialDecay, **twitter_stream_settings )
         streamObject=streamIdToStreamObjectMap[message.streamId]
-        print streamObject
+#        print streamObject
+        GeneralMethods.callMethodEveryInterval(TwitterCrowdsSpecificMethods.printParam, timedelta(minutes=15), message.timeStamp, param=message.timeStamp)
         
     print len(phraseTextToIdMap)
     print len(phraseTextToPhraseObjectMap)
