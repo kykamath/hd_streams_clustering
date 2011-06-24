6d7d45564dd8c0ad23fa645955b600a20f901316
Krishna Kamath <krishna.kamath@gmail.com>
2011-06-24 18:47:25 -0500

    added pruning

--- settings.py
+++ settings.py
@@ -8,8 +8,8 @@ from datetime import timedelta
 
 twitter_stream_settings = Settings(
                                    max_dimensions=100000, # Number of maximum dimensions to consider at a time. This is also equal to the number of top phrases that will be considered for crowd discovery.
-                                   min_phrase_length=2, # Minumum lenght of phrases. For example min_phrase_length=1 and max_phrase_length=1 will result in only unigrams as features.
-                                   max_phrase_length=4, # Mzximum lenght of phrases. For example min_phrase_length=1 and max_phrase_length=2 will result in both unigrams and bigrams as features.
+                                   min_phrase_length=1, # Minumum lenght of phrases. For example min_phrase_length=1 and max_phrase_length=1 will result in only unigrams as features.
+                                   max_phrase_length=2, # Mzximum lenght of phrases. For example min_phrase_length=1 and max_phrase_length=2 will result in both unigrams and bigrams as features.
                                    
                                    phrase_decay_coefficient=0.5, # The rate at which phrases decays.
                                    stream_decay_coefficient=0.5, # The rate at which stream decays
