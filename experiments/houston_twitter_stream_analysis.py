638a4ae308ece309b80841f07b1dcff5b1dd32e4
kykamath <krishna.kamath@gmail.com>
2011-06-30 22:59:44 -0500

    modified houston code

--- experiments/houston_twitter_stream_analysis.py
+++ experiments/houston_twitter_stream_analysis.py
@@ -7,11 +7,12 @@ from pymongo import Connection
 class GenerateData:
     @staticmethod
     def generateHoustonFiles():
+        userIdToScreenNameMap = {}
         mongodb_connection = Connection('sarge', 27017)
         tweets = mongodb_connection.old_hou.Tweet
         users = mongodb_connection.old_hou.User
         for tweet in tweets.find(limit=10):
-            print tweet
+            print tweet['ca'], tweet['tx']
 
 if __name__ == '__main__':
     GenerateData.generateHoustonFiles()
