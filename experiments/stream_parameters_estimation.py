67078331395e5802c9cd3bf34a01f058136e2072
Krishna Kamath <krishna.kamath@gmail.com>
2011-07-07 23:15:38 -0500

    minor modifications

--- experiments/stream_parameters_estimation.py
+++ experiments/stream_parameters_estimation.py
@@ -315,8 +315,8 @@ class ClusteringParametersEstimation():
                          ClusteringParametersEstimation.clusterLagDistributionId: lagDistribution,
                          'lag_between_streams_added_to_cluster': hdStreamClusteringObject.stream_settings['lag_between_streams_added_to_cluster']
                          }
-        print hdStreamClusteringObject.stream_settings['lag_between_streams_added_to_cluster']
-#        FileIO.writeToFileAsJson(iterationData, hdStreamClusteringObject.stream_settings['%s_file'%ClusteringParametersEstimation.clusterLagDistributionId])
+#        print hdStreamClusteringObject.stream_settings['lag_between_streams_added_to_cluster']
+        FileIO.writeToFileAsJson(iterationData, hdStreamClusteringObject.stream_settings['%s_file'%ClusteringParametersEstimation.clusterLagDistributionId])
             
     @staticmethod
     def emptyClusterFilteringMethod(hdStreamClusteringObject, currentMessageTime): pass
@@ -356,8 +356,8 @@ def clusterDecayEstimation():
     experts_twitter_stream_settings['cluster_analysis_method'] = houston_twitter_stream_settings['cluster_analysis_method'] = ClusteringParametersEstimation.clusterLagDistributionMethod
     experts_twitter_stream_settings['lag_between_streams'] = houston_twitter_stream_settings['lag_between_streams'] = analyzeClusterLag
     experts_twitter_stream_settings['lag_between_streams_added_to_cluster']=houston_twitter_stream_settings['lag_between_streams_added_to_cluster']=defaultdict(int)
-#    ClusteringParametersEstimation(**experts_twitter_stream_settings).run(TwitterIterators.iterateTweetsFromExperts())
-    ClusteringParametersEstimation(**houston_twitter_stream_settings).run(TwitterIterators.iterateTweetsFromHouston())
+    ClusteringParametersEstimation(**experts_twitter_stream_settings).run(TwitterIterators.iterateTweetsFromExperts())
+#    ClusteringParametersEstimation(**houston_twitter_stream_settings).run(TwitterIterators.iterateTweetsFromHouston())
 
 if __name__ == '__main__':
 #    dimensionsEstimation()
