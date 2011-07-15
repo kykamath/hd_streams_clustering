2f32b5fdd9b350b587566df81dd5bd62300476e5
Krishna Kamath <krishna.kamath@gmail.com>
2011-07-15 17:45:02 -0500

    modified experiments

--- experiments/mr_clustering_quality.py
+++ experiments/mr_clustering_quality.py
@@ -19,8 +19,8 @@ def extractArraysFromFile(file, percentage=1.0):
 
 length=100
 localFileName = clustering_quality_experts_mr_folder+'%s'%length
-localFileName = hdfsPath++'%s'%length
-clusters = list(KMeans.cluster(localFileName, extractArraysFromFile(localFileName,0.9), mrArgs='-r hadoop', iterations=1, jobconf={'mapred.map.tasks':10}))
+hdfsFileName = hdfsPath++'%s'%length
+clusters = list(KMeans.cluster(hdfsFileName, extractArraysFromFile(localFileName,0.9), mrArgs='-r hadoop', iterations=1, jobconf={'mapred.map.tasks':10}))
 print clusters
 distribution = defaultdict(int)
 for clusterId, cluster in clusters:
