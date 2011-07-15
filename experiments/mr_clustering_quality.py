c84ccb17086343ec279f9d0058e6d8d2c4058b8e
Krishna Kamath <krishna.kamath@gmail.com>
2011-07-15 14:25:50 -0500

    modified experiments

--- experiments/mr_clustering_quality.py
+++ experiments/mr_clustering_quality.py
@@ -6,17 +6,20 @@ Created on Jul 15, 2011
 from library.file_io import FileIO
 from library.mr_algorithms.kmeans import KMeans
 import numpy as np
+from collections import defaultdict
 
 clustering_quality_experts_folder = '/mnt/chevron/kykamath/data/twitter/lsh_clustering/clustering_quality_experts_folder/'
 clustering_quality_experts_mr_folder = clustering_quality_experts_folder+'mr_data/'
 
-def extractArraysFromFile(file, numberOfArrays=float('+inf')):
+def extractArraysFromFile(file, percentage=1.0):
         arraysToReturn = []
-        for line in FileIO.iterateJsonFromFile(file):
-            if numberOfArrays==0: break;
-            else: numberOfArrays-=1
-            arraysToReturn.append(np.array(line['vector']))
-        return arraysToReturn
+        for line in FileIO.iterateJsonFromFile(file): arraysToReturn.append(np.array(line['vector']))
+        return arraysToReturn[:int(len(arraysToReturn)*percentage)]
+
 fileName = clustering_quality_experts_mr_folder+'1000'
-#    print fileName
-print list(KMeans.cluster(fileName, extractArraysFromFile(fileName), mrArgs='-r hadoop', iterations=5))
+clusters = list(KMeans.cluster(fileName, extractArraysFromFile(fileName,0.9), mrArgs='-r hadoop', iterations=5))
+print clusters
+distribution = defaultdict(int)
+for clusterId, cluster in clusters:
+    distribution[len(cluster)]+=1
+print sorted(distribution)
