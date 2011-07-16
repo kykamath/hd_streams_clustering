0c7a99b474f80a0417a3d125ed71c9b06dd674cb
Krishna Kamath <krishna.kamath@gmail.com>
2011-07-16 18:59:32 -0500

    added sst modules

---
+++ experiments/quality_comparison_with_similar_stream_aggregation.py
@@ -0,0 +1,63 @@
+'''
+Created on Jul 16, 2011
+
+@author: kykamath
+'''
+from itertools import combinations
+from collections import defaultdict
+from twitter_streams_clustering import getExperts
+from settings import experts_twitter_stream_settings
+
+clustering_quality_experts_folder = '/mnt/chevron/kykamath/data/twitter/lsh_clustering/clustering_quality_ssa_folder/'
+#clustering_quality_experts_mr_folder = clustering_quality_experts_folder+'mr_data/'
+#hdfsPath='hdfs:///user/kykamath/lsh_experts_data/ssa'
+
+class ItemsClusterer:
+    def __init__(self):
+        self.clusters = defaultdict(set)
+        self.itemToClusterMap = {}
+    def add(self, items):
+        possibleClusters = set([self.itemToClusterMap.get(k) for k in items if k in self.itemToClusterMap])
+        if not possibleClusters: return self.addToExistingCluster(list(items)[0], items)
+        elif len(possibleClusters)==1: return self.addToExistingCluster(list(possibleClusters)[0], items)
+        else: return self.mergeClusters(possibleClusters, items)
+        raise Exception('Code shouldn\'t come here')
+    def addToExistingCluster(self, clusterId, items):
+        self.clusters[clusterId]=self.clusters[clusterId].union(items)
+        for item in items: self.itemToClusterMap[item]=clusterId
+    def mergeClusters(self, clusterIds, items):
+        clusterIdsList=list(clusterIds)
+        mainClusterId = clusterIdsList[0]
+        self.addToExistingCluster(mainClusterId, items)
+        for clusterId in clusterIdsList[1:]: 
+            self.addToExistingCluster(mainClusterId, self.clusters[clusterId])
+            del self.clusters[clusterId]
+    def iterateClusters(self):
+        for cluster in self.clusters.values(): yield cluster
+
+class SimilarStreamAggregation:
+    def __init__(self, vectors, epsilon): self.vectors, self.epsilon, self.clusterer = vectors, epsilon, ItemsClusterer()
+    def estimate(self):
+        similarVectorMap = self._getVectorsWithinEpsilon()
+        for k, v in similarVectorMap.iteritems(): self.clusterer.add(set([k]).union(v))
+    def iterateClusters(self): return self.clusterer.iterateClusters()
+    def _getVectorsWithinEpsilon(self):
+        vectorsWithinEpsilon = defaultdict(set)
+        for id1, id2 in combinations(self.vectors,2):
+            if  self.vectors[id1].cosineSimilarity(self.vectors[id2])>=self.epsilon: vectorsWithinEpsilon[id1].add(id2), vectorsWithinEpsilon[id2].add(id1)
+        return vectorsWithinEpsilon
+    
+class TweetsFile:
+    stats_file = clustering_quality_experts_folder+'quality_stats'
+    def __init__(self, length, **stream_settings):
+        self.length=length
+        self.stream_settings = stream_settings
+        self.fileName = clustering_quality_experts_folder+'data/'+str(length)
+        self.expertsToClassMap = dict([(k, v['class']) for k,v in getExperts(byScreenName=True).iteritems()])
+    def getStatsForSST(self):
+        print self.fileName
+    
+if __name__ == '__main__':
+    TweetsFile(1000, **experts_twitter_stream_settings).getStatsForSST()
+        
+    
