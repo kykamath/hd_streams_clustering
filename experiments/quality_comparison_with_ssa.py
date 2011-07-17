c8c88fa952e434019bb450135c423eb131e9e29b
Krishna Kamath <krishna.kamath@gmail.com>
2011-07-17 09:46:21 -0500

    modiifed experiments

--- experiments/quality_comparison_with_ssa.py
+++ experiments/quality_comparison_with_ssa.py
@@ -64,8 +64,8 @@ class TweetsFile:
         length=1000
         tf = TweetsFile(2000, **experts_twitter_stream_settings)
         iteration_file = clustering_quality_experts_ssa_mr_folder+str(length)
+        print 'Writing data to ', iteration_file
         with open(iteration_file, 'w') as fp: [fp.write(CJSONProtocol.write('x', [doc1, doc2])+'\n') for doc1, doc2 in combinations(tf._iterateUserDocuments(),2)]
-        print iteration_file
 if __name__ == '__main__':
     experts_twitter_stream_settings['ssa_threshold']=0.75
     TweetsFile.generateDocsForSSAMR()
