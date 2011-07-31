e769aeedf6536eb9acc8a1259a6ce7cf6a6618c5
Krishna Kamath <krishna.kamath@gmail.com>
2011-07-31 14:50:57 -0500

    modified experiments

--- experiments/quality_comparison_with_ssa.py
+++ experiments/quality_comparison_with_ssa.py
@@ -176,10 +176,10 @@ class QualityComparisonWithSSA:
         
 if __name__ == '__main__':
     experts_twitter_stream_settings['ssa_threshold']=0.75
-    TweetsFile.generateDocsForSSAMR()
+#    TweetsFile.generateDocsForSSAMR()
 #    TweetsFile.copyUnzippedSSADataToHadoop()
 
 #    QualityComparisonWithSSA.generateStatsForQualityComparisonWithSSA()
 #    QualityComparisonWithSSA.plotClusteringSpeed()
-#    QualityComparisonWithSSA.plotClusteringQuality()
+    QualityComparisonWithSSA.plotClusteringQuality()
 #    QualityComparisonWithSSA.plotQualityWithKMeansAndSSA()
