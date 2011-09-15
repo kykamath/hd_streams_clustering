b70b30439991a07f3b7f0c359bcb079bda483b93
Krishna Kamath <krishna.kamath@gmail.com>
2011-09-15 12:41:08 -0500

    moidifed experiemtns

--- experiments/plots.py
+++ experiments/plots.py
@@ -5,10 +5,35 @@ Created on Sep 14, 2011
 '''
 from experiments.quality_comparison_with_kmeans import TweetsFile
 from library.file_io import FileIO
+import matplotlib.pyplot as plt
+import numpy as np
 
 def iterateData():
     for nonOptimzed, optimized in zip(FileIO.iterateJsonFromFile(TweetsFile.default_stats_file), FileIO.iterateJsonFromFile(TweetsFile.stats_file)): yield nonOptimzed, optimized
 
-for nonOptimzed, optimized in iterateData():
-    print 'non-opt', nonOptimzed['streaming_lsh']['iteration_time'], nonOptimzed['streaming_lsh']['nmi'], nonOptimzed['streaming_lsh']['no_of_documents'], nonOptimzed['settings']['stream_id']
-    print 'opt', optimized['streaming_lsh']['iteration_time'], optimized['streaming_lsh']['nmi'], optimized['streaming_lsh']['no_of_documents'], optimized['settings']['stream_id']
+#for nonOptimzed, optimized in iterateData():
+#    print 'non-opt', nonOptimzed['streaming_lsh']['iteration_time'], nonOptimzed['streaming_lsh']['nmi'], nonOptimzed['streaming_lsh']['no_of_documents'], nonOptimzed['settings']['stream_id']
+#    print 'opt', optimized['streaming_lsh']['iteration_time'], optimized['streaming_lsh']['nmi'], optimized['streaming_lsh']['no_of_documents'], optimized['settings']['stream_id']
+    
+def plotTime():
+    dataX, optTime, unOptTime = [], [], []
+    for nonOptimzed, optimized in iterateData():
+        dataX.append(optimized['streaming_lsh']['no_of_documents'])
+        optTime.append(optimized['streaming_lsh']['iteration_time'])
+        unOptTime.append(nonOptimzed['streaming_lsh']['iteration_time'])
+#        print 'non-opt', nonOptimzed['streaming_lsh']['iteration_time'], nonOptimzed['streaming_lsh']['nmi'], nonOptimzed['streaming_lsh']['no_of_documents'], nonOptimzed['settings']['stream_id']
+#        print 'opt', optimized['streaming_lsh']['iteration_time'], optimized['streaming_lsh']['nmi'], optimized['streaming_lsh']['no_of_documents'], optimized['settings']['stream_id']
+    plt.plot(dataX, optTime, label='opt')
+    plt.plot(dataX, unOptTime, label='un-opt')
+    plt.legend()
+    plt.show()
+
+def plotQuality():
+    optQuality, unOptQuality = [], []
+    for nonOptimzed, optimized in iterateData():
+        optQuality.append(optimized['streaming_lsh']['nmi'])
+        unOptQuality.append(nonOptimzed['streaming_lsh']['nmi'])
+    print 'opt', np.mean(optQuality)
+    print 'un opt', np.mean(unOptQuality)
+if __name__ == '__main__':
+    plotQuality()
