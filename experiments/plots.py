31701d7cc413df844a90d67e57c94c1b4ca0181a
Krishna Kamath <krishna.kamath@gmail.com>
2011-09-15 22:55:42 -0500

    modified experiments

--- experiments/plots.py
+++ experiments/plots.py
@@ -9,7 +9,7 @@ from library.file_io import FileIO
 import matplotlib.pyplot as plt
 from hd_streams_clustering import HDStreaminClustering
 import numpy as np
-from settings import default_experts_twitter_stream_settings
+from settings import default_experts_twitter_stream_settings, experts_twitter_stream_settings
 from library.clustering import EvaluationMetrics
 from twitter_streams_clustering import TwitterIterators, getExperts,\
     TwitterCrowdsSpecificMethods
@@ -23,27 +23,16 @@ def iterateData():
 #    for nonOptimzed, optimized in zip(FileIO.iterateJsonFromFile(TweetsFile.default_stats_file), FileIO.iterateJsonFromFile(TweetsFile.stats_file)): yield nonOptimzed, optimized
     for nonOptimzed, optimized in zip(FileIO.iterateJsonFromFile('default_stats_file'), FileIO.iterateJsonFromFile('quality_stats')): yield nonOptimzed, optimized
 
-default_experts_twitter_stream_settings['convert_data_to_message_method'] = TwitterCrowdsSpecificMethods.convertTweetJSONToMessage
-default_experts_twitter_stream_settings['cluster_analysis_method'] = emptyClusterAnalysisMethod
-
 class TweetsFile:
-    stats_file = clustering_quality_experts_folder+'quality_stats'
-    default_stats_file = clustering_quality_experts_folder+'default_stats_file'
+    optimized_stats_file = clustering_quality_experts_folder+'optimized_stats_file'
+    unoptomized_stats_file = clustering_quality_experts_folder+'unoptomized_stats_file'
     def __init__(self, length, forGeneration=False, **stream_settings):
         self.length=length
         self.stream_settings = stream_settings
+        self.stream_settings['convert_data_to_message_method'] = TwitterCrowdsSpecificMethods.convertTweetJSONToMessage
+        self.stream_settings['cluster_analysis_method'] = emptyClusterAnalysisMethod
         self.fileName = clustering_quality_experts_folder+'data/'+str(length)
         self.expertsToClassMap = dict([(k, v['class']) for k,v in getExperts(byScreenName=True).iteritems()])
-#        if not forGeneration: self.documents = list(self._tweetIterator())
-#    def _tweetIterator(self):
-#            userMap = {}
-#            for tweet in TwitterIterators.iterateFromFile(self.fileName+'.gz'):
-#                user = tweet['user']['screen_name']
-#                phrases = [phrase.replace(' ', unique_string) for phrase in getPhrases(getWordsFromRawEnglishMessage(tweet['text']), self.stream_settings['min_phrase_length'], self.stream_settings['max_phrase_length'])]
-#                if phrases:
-#                    if user not in userMap: userMap[user] = ' '.join(phrases)
-#                    else: userMap[user]+= ' ' + ' '.join(phrases)
-#            return userMap.iteritems()
     def _getExpertClasses(self, cluster): return [self.expertsToClassMap[user.lower()] for user in cluster if user.lower() in self.expertsToClassMap]
     def getEvaluationMetrics(self, documentClusters, timeDifference):
         iterationData =  {'no_of_documents':self.length, 'no_of_clusters': len(documentClusters), 'iteration_time': timeDifference, 'clusters': documentClusters}
@@ -91,7 +80,7 @@ if __name__ == '__main__':
 #    plotQuality()
 #    plotTime()
 
-    tf = TweetsFile(10000, **default_experts_twitter_stream_settings)
+    tf = TweetsFile(10000, **experts_twitter_stream_settings)
 #    for i in TwitterIterators.iterateFromFile(tf.fileName+'.gz'):
 #        print TwitterCrowdsSpecificMethods.convertTweetJSONToMessage(i, **default_experts_twitter_stream_settings)
     print tf.generateStatsForStreamingLSHClustering()
