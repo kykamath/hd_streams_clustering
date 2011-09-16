cf5aaba3c3b08ff53f0935b6d915a429064f601c
Krishna Kamath <krishna.kamath@gmail.com>
2011-09-16 12:19:14 -0500

    modified experiments

--- hd_streams_clustering.py
+++ hd_streams_clustering.py
@@ -67,17 +67,18 @@ class HDStreaminClustering(StreamingLSHClustering):
         for data in dataIterator:
 #            message = self.convertDataToMessageMethod(data, **self.stream_settings)
             message = data
-            if DataStreamMethods.messageInOrder(message.timeStamp):
+#            if DataStreamMethods.messageInOrder(message.timeStamp):
 #                UtilityMethods.updatePhraseTextToPhraseObject(message.vector, message.timeStamp, self.phraseTextToPhraseObjectMap, **self.stream_settings)
-                if message.streamId not in self.streamIdToStreamObjectMap: self.streamIdToStreamObjectMap[message.streamId] = Stream(message.streamId, message)
+#                if message.streamId not in self.streamIdToStreamObjectMap: self.streamIdToStreamObjectMap[message.streamId] = Stream(message.streamId, message)
 #                else: self.streamIdToStreamObjectMap[message.streamId].updateForMessage(message, VectorUpdateMethods.exponentialDecay, **self.stream_settings )
-                streamObject=self.streamIdToStreamObjectMap[message.streamId]
+#                streamObject=self.streamIdToStreamObjectMap[message.streamId]
 #                self.updateDimensionsMethod.call(message.timeStamp, hdStreamClusteringObject=self, currentMessageTime=message.timeStamp)
 #                self.clusterFilteringMethod.call(message.timeStamp, hdStreamClusteringObject=self, currentMessageTime=message.timeStamp)
 #                self.clusterAnalysisMethod.call(message.timeStamp, hdStreamClusteringObject=self, currentMessageTime=message.timeStamp)
-                i+=1
+#                i+=1
 #                print i, streamObject.lastMessageTime
-                self.getClusterAndUpdateExistingClusters(streamObject)
+#            self.getClusterAndUpdateExistingClusters(streamObject)
+            self.getClusterAndUpdateExistingClusters(message)
 #    def getClusterAndUpdateExistingClusters(self, stream):
 #        predictedCluster = self.getClusterForDocument(stream)
 #        '''
