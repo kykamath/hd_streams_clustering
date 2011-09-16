58cbfd12989f7b5cf0cfecadfecf07c451e65f69
Krishna Kamath <krishna.kamath@gmail.com>
2011-09-16 12:24:22 -0500

    modified experiments

--- hd_streams_clustering.py
+++ hd_streams_clustering.py
@@ -67,7 +67,7 @@ class HDStreaminClustering(StreamingLSHClustering):
         for data in dataIterator:
 #            message = self.convertDataToMessageMethod(data, **self.stream_settings)
             message = data
-#            if DataStreamMethods.messageInOrder(message.timeStamp):
+            if DataStreamMethods.messageInOrder(message.timeStamp):
 #                UtilityMethods.updatePhraseTextToPhraseObject(message.vector, message.timeStamp, self.phraseTextToPhraseObjectMap, **self.stream_settings)
 #                if message.streamId not in self.streamIdToStreamObjectMap: self.streamIdToStreamObjectMap[message.streamId] = Stream(message.streamId, message)
 #                else: self.streamIdToStreamObjectMap[message.streamId].updateForMessage(message, VectorUpdateMethods.exponentialDecay, **self.stream_settings )
@@ -78,7 +78,7 @@ class HDStreaminClustering(StreamingLSHClustering):
 #                i+=1
 #                print i, streamObject.lastMessageTime
 #            self.getClusterAndUpdateExistingClusters(streamObject)
-            self.getClusterAndUpdateExistingClusters(message)
+                self.getClusterAndUpdateExistingClusters(message)
 #    def getClusterAndUpdateExistingClusters(self, stream):
 #        predictedCluster = self.getClusterForDocument(stream)
 #        '''
