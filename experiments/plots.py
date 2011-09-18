9d42a9af34557d0bf5b0d5f0e44e8b9ad3123dd8
Krishna Kamath <krishna.kamath@gmail.com>
2011-09-17 19:23:40 -0500

    modified experiments

--- experiments/plots.py
+++ experiments/plots.py
@@ -29,102 +29,14 @@ hd_clustering_performance_folder = '/mnt/chevron/kykamath/data/twitter/lsh_clust
 experts_twitter_stream_settings['status_file'] = clustering_quality_hd_experiments_folder+'optimized_stats_file'
 default_experts_twitter_stream_settings['status_file'] = clustering_quality_hd_experiments_folder+'unoptomized_stats_file'
 
-#def iterateData():
-#    for nonOptimzed, optimized in zip(FileIO.iterateJsonFromFile(TweetsFile.default_stats_file), FileIO.iterateJsonFromFile(TweetsFile.stats_file)): yield nonOptimzed, optimized
-#    for nonOptimzed, optimized in zip(FileIO.iterateJsonFromFile('default_stats_file'), FileIO.iterateJsonFromFile('quality_stats')): yield nonOptimzed, optimized
-
-#class TweetsFile:
-#    def __init__(self, length, forGeneration=False, **stream_settings):
-#        self.length=length
-#        self.stream_settings = stream_settings
-#        self.stream_settings['convert_data_to_message_method'] = TwitterCrowdsSpecificMethods.convertTweetJSONToMessage
-#        self.stream_settings['cluster_analysis_method'] = emptyClusterAnalysisMethod
-#        self.stream_settings['cluster_filtering_method'] = emptyClusterFilteringMethod
-#        self.fileName = clustering_quality_experts_folder+'data/'+str(length)
-#        self.expertsToClassMap = dict([(k, v['class']) for k,v in getExperts(byScreenName=True).iteritems()])
-#    def _getExpertClasses(self, cluster): return [self.expertsToClassMap[user.lower()] for user in cluster if user.lower() in self.expertsToClassMap]
-#    def getEvaluationMetrics(self, documentClusters, timeDifference):
-#        iterationData =  {'no_of_documents':self.length, 'no_of_clusters': len(documentClusters), 'iteration_time': timeDifference, 'clusters': documentClusters}
-#        clustersForEvaluation = [self._getExpertClasses(cluster) for cluster in documentClusters]
-#        iterationData['nmi'] = EvaluationMetrics.getValueForClusters(clustersForEvaluation, EvaluationMetrics.nmi)
-#        iterationData['purity'] = EvaluationMetrics.getValueForClusters(clustersForEvaluation, EvaluationMetrics.purity)
-#        iterationData['f1'] = EvaluationMetrics.getValueForClusters(clustersForEvaluation, EvaluationMetrics.f1)
-#        return iterationData
-#    def generateStatsForStreamingLSHClustering(self):
-#        def getDocuments():
-#            documents = []
-#            for data in TwitterIterators.iterateFromFile(self.fileName+'.gz'): documents.append(TwitterCrowdsSpecificMethods.convertTweetJSONToMessage(data, **self.stream_settings))
-#            return documents
-#        documents = getDocuments()
-#        clustering=HDStreaminClustering(**self.stream_settings)
-#        ts = time.time()
-#        clustering.cluster(documents)
-#        te = time.time()
-#        documentClusters = [cluster.documentsInCluster.keys() for k, cluster in clustering.clusters.iteritems() if len(cluster.documentsInCluster.keys())>=self.stream_settings['cluster_filter_threshold']]
-#        return self.getEvaluationMetrics(documentClusters, te-ts)
-#    def generateStatsForHDLSHClustering(self):
-#        print 'HD LSH'
-#        def getDocuments():
-#            documents = []
-#            for data in TwitterIterators.iterateFromFile(self.fileName+'.gz'): 
-#                message = TwitterCrowdsSpecificMethods.convertTweetJSONToMessage(data, **self.stream_settings)
-#                documents.append(Stream(message.streamId, message))
-#            return documents
-#        documents = getDocuments()
-#        clustering=HDStreaminClustering(**self.stream_settings)
-#        ts = time.time()
-#        for tweet in documents: clustering.getClusterAndUpdateExistingClusters(tweet)
-#        te = time.time()
-#        documentClusters = [cluster.documentsInCluster.keys() for k, cluster in clustering.clusters.iteritems() if len(cluster.documentsInCluster.keys())>=self.stream_settings['cluster_filter_threshold']]
-#        return self.getEvaluationMetrics(documentClusters, te-ts)
-#    @staticmethod
-#    def generateStatsFor(streamSettings):
-#        for i in [10**3, 10**4, 10**5]: 
-#            for j in range(1, 10):
-#                print 'Exerpiments for:', i*j
-#                tf = TweetsFile(i*j, **streamSettings)
-#                iteration_data = tf.generateStatsForHDLSHClustering()
-#                FileIO.writeToFileAsJson({'iteration_data': iteration_data, 
-#                                          'settings': Settings.getSerialzedObject(tf.stream_settings)}, 
-#                                          streamSettings['status_file'])
-#                print iteration_data
-
-#def plotTime():
-#    dataX, optTime, unOptTime = [], [], []
-#    for nonOptimzed, optimized in iterateData():
-#        dataX.append(optimized['streaming_lsh']['no_of_documents'])
-#        optTime.append(optimized['streaming_lsh']['iteration_time'])
-#        unOptTime.append(nonOptimzed['streaming_lsh']['iteration_time'])
-##        print 'non-opt', nonOptimzed['streaming_lsh']['iteration_time'], nonOptimzed['streaming_lsh']['nmi'], nonOptimzed['streaming_lsh']['no_of_documents'], nonOptimzed['settings']['stream_id']
-##        print 'opt', optimized['streaming_lsh']['iteration_time'], optimized['streaming_lsh']['nmi'], optimized['streaming_lsh']['no_of_documents'], optimized['settings']['stream_id']
-#    plt.plot(dataX, optTime, label='opt')
-#    plt.plot(dataX, unOptTime, label='un-opt')
-#    plt.legend()
-#    plt.savefig('plt_time.eps')
-#
-#def plotQuality():
-#    dataX, optQuality, unOptQuality = [], [], []
-#    for nonOptimzed, optimized in iterateData():
-#        dataX.append(optimized['streaming_lsh']['no_of_documents'])
-#        optQuality.append(optimized['streaming_lsh']['nmi'])
-#        unOptQuality.append(nonOptimzed['streaming_lsh']['nmi'])
-##    print 'opt', np.mean(optQuality)
-##    print 'un opt', np.mean(unOptQuality)
-#    plt.plot(dataX, optQuality, label='opt')
-#    plt.plot(dataX, unOptQuality, label='un-opt')
-#    plt.legend()
-#    plt.savefig('plt_quality.eps')
-
-
-CDA = 'cda'
 
 algorithm_info = {
                   'cda': {'label': 'opt', 'color': 'k'},
-                  'cda_unopt': {'label': 'unopt', 'color': 'b'},
+                  'cda_unopt': {'label': 'Streaming-CDA', 'color': '#7109AA'},
                   'cda_it': {'label': 'cda_it', 'color': 'm'},
                   'cda_mr': {'label': 'cda_mr', 'color': 'y'},
-                  'kmeans': {'label': 'kmeans', 'color': 'r'},
-                  'kmeans_mr': {'label': 'kmeans_mr', 'color': 'g'}
+                  'kmeans': {'label': 'k-Means', 'color': '#FD0006'},
+                  'kmeans_mr': {'label': 'MR k-Means', 'color': '#5AF522'}
                   }
 
 class DataIterators:
@@ -149,16 +61,17 @@ class DataIterators:
 
 class CompareAlgorithms:
     @staticmethod
-    def runningTimes(*iterators):
+    def runningTimes(*iterators, **kwargs):
+        loc = kwargs.get('loc', 1)
+        fileName = kwargs.get('file_name', 'running_times.eps')
         for id, iterator in iterators:
             dataX, dataY = [], []
             for data in iterator:
                 dataX.append(data['no_of_documents']), dataY.append(data['iteration_time'])
-            plt.plot(dataX, dataY, label=algorithm_info[id]['label'], color=algorithm_info[id]['color'])
-        plt.legend()
-#        plt.show()
-        plt.savefig('running_times.eps')
-    
+            plt.plot(dataX, dataY, label=algorithm_info[id]['label'], color=algorithm_info[id]['color'], lw=2)
+        plt.legend(loc=loc)
+        plt.savefig(fileName)
+        
     @staticmethod
     def quality(quality_type, *iterators):
         for id, iterator in iterators:
@@ -172,12 +85,17 @@ if __name__ == '__main__':
 #    TweetsFile.generateStatsFor(experts_twitter_stream_settings)
 #    TweetsFile.generateStatsFor(default_experts_twitter_stream_settings)
 
-#    CompareAlgorithms.runningTimes(('kmeans', DataIterators.kmeans()), ('cda', DataIterators.optimized()), ('kmeans_mr', DataIterators.kmeansmr()), ('cda_unopt', DataIterators.unoptimized()))
+    CompareAlgorithms.runningTimes(
+                                   ('kmeans', DataIterators.kmeans()), 
+                                   ('kmeans_mr', DataIterators.kmeansmr()), 
+                                   ('cda_unopt', DataIterators.unoptimized()),
+                                   loc=3, file_name='running_time_kmeans.eps'
+                                )
 #    CompareAlgorithms.runningTimes(('cda_it', DataIterators.cdait()), ('cda', DataIterators.optimized()), ('cda_mr', DataIterators.cdamr()), ('cda_unopt', DataIterators.unoptimized()))
 #    CompareAlgorithms.runningTimes(('cda', DataIterators.optimized()), ('cda_unopt', DataIterators.unoptimized()))
 
-    CompareAlgorithms.quality('purity', ('cda', DataIterators.optimized()), ('cda_unopt', DataIterators.unoptimized()))
-    CompareAlgorithms.quality('nmi', ('cda', DataIterators.optimized()), ('cda_unopt', DataIterators.unoptimized()))
+#    CompareAlgorithms.quality('purity', ('cda', DataIterators.optimized()), ('cda_unopt', DataIterators.unoptimized()))
+#    CompareAlgorithms.quality('nmi', ('cda', DataIterators.optimized()), ('cda_unopt', DataIterators.unoptimized()))
 #    for d in DataIterators.cdamr(): 
 #        del d['clusters']
 #        print d
