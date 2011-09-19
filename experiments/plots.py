413b8629f89ee1ecccf16c625a5e15e0d1855496
Krishna Kamath <krishna.kamath@gmail.com>
2011-09-18 20:12:54 -0500

    modified experimetns

--- experiments/plots.py
+++ experiments/plots.py
@@ -3,21 +3,14 @@ Created on Sep 14, 2011
 
 @author: kykamath
 '''
-import sys, os, time
-from library.plotting import getLatexForString
-sys.path.append('../')
-from classes import Stream
 from library.file_io import FileIO
+from library.plotting import getLatexForString
+from settings import default_experts_twitter_stream_settings, \
+    experts_twitter_stream_settings
 import matplotlib.pyplot as plt
-from hd_streams_clustering import HDStreaminClustering
 import numpy as np
-from settings import default_experts_twitter_stream_settings, experts_twitter_stream_settings
-from library.clustering import EvaluationMetrics
-from twitter_streams_clustering import TwitterIterators, getExperts,\
-    TwitterCrowdsSpecificMethods
-from library.nlp import getPhrases, getWordsFromRawEnglishMessage
-from algorithms_performance import emptyClusterAnalysisMethod, emptyClusterFilteringMethod
-from settings import Settings
+import sys
+sys.path.append('../')
 
 OPTIMIZED_ID = 'optimized'
 UN_OPTIMIZED_ID = 'un_optimized'
@@ -32,12 +25,12 @@ default_experts_twitter_stream_settings['status_file'] = clustering_quality_hd_e
 
 
 algorithm_info = {
-                  'cda': {'label': 'Optimized Streaming-CDA', 'color': '#56F2F5'},
-                  'cda_unopt': {'label': 'Streaming-CDA', 'color': '#7109AA'},
-                  'cda_it': {'label': 'Iterative CDA', 'color': '#FD0006'},
-                  'cda_mr': {'label': 'MR CDA', 'color': '#5AF522'},
-                  'kmeans': {'label': 'k-Means', 'color': '#FD0006'},
-                  'kmeans_mr': {'label': 'MR k-Means', 'color': '#5AF522'}
+                  'cda': {'label': 'Optimized Streaming-CDA', 'color': '#56F2F5', 'marker': 's'},
+                  'cda_unopt': {'label': 'Streaming-CDA', 'color': '#7109AA', 'marker': '*'},
+                  'cda_it': {'label': 'Iterative CDA', 'color': '#FD0006', 'marker': 'o'},
+                  'cda_mr': {'label': 'MR CDA', 'color': '#5AF522', 'marker': '>'},
+                  'kmeans': {'label': 'k-Means', 'color': '#FD0006', 'marker': 'o'},
+                  'kmeans_mr': {'label': 'MR k-Means', 'color': '#5AF522', 'marker': '>'}
                   }
 
 class DataIterators:
@@ -66,7 +59,7 @@ class CompareAlgorithms:
         loc = kwargs.get('loc', 1)
         fileName = kwargs.get('file_name', 'running_times.eps')
         xmax = kwargs.get('xmax', None)
-        log = kwargs.get('log', True)
+        semilog = kwargs.get('log', False)
         xmin = kwargs.get('xmin', None)
         title = kwargs.get('title', None)
         for id, iterator in iterators:
@@ -74,8 +67,8 @@ class CompareAlgorithms:
             for data in iterator:
                 if xmax and data['no_of_documents'] <= xmax: dataX.append(data['no_of_documents']), dataY.append(data['iteration_time'])
                 else: dataX.append(data['no_of_documents']), dataY.append(data['iteration_time'])
-            if log: plt.loglog(dataX, dataY, label=algorithm_info[id]['label'], color=algorithm_info[id]['color'], lw=2)
-            else: plt.plot(dataX, dataY, label=algorithm_info[id]['label'], color=algorithm_info[id]['color'], lw=2)
+            if not semilog: plt.loglog(dataX, dataY, label=algorithm_info[id]['label'], color=algorithm_info[id]['color'], lw=2, marker=algorithm_info[id]['marker'])
+            else: plt.semilogx(dataX, dataY, label=algorithm_info[id]['label'], color=algorithm_info[id]['color'], lw=2, marker=algorithm_info[id]['marker'])
         plt.legend(loc=loc)
         plt.xlabel(getLatexForString('\# of documents')); plt.ylabel(getLatexForString('Running time (s)')); plt.title(getLatexForString(title))
         if xmax: plt.xlim(xmax=xmax) 
@@ -83,19 +76,51 @@ class CompareAlgorithms:
         plt.savefig(fileName)
         
     @staticmethod
-    def quality(quality_type, *iterators):
+    def runningTimesWithCDA(*iterators, **kwargs):
+        loc = kwargs.get('loc', 1)
+        fileName = kwargs.get('file_name', 'running_times.eps')
+        xmax = kwargs.get('xmax', None)
+        semilog = kwargs.get('log', False)
+        xmin = kwargs.get('xmin', None)
+        title = kwargs.get('title', None)
+        for id, iterator in iterators:
+            dataX, dataY = [], []
+            for data in iterator:
+                if xmax and data['no_of_documents'] <= xmax: dataX.append(data['no_of_documents']), dataY.append(data['iteration_time'])
+                else: dataX.append(data['no_of_documents']), dataY.append(data['iteration_time'])
+            if not semilog: plt.loglog(dataX, dataY, label=algorithm_info[id]['label'], color=algorithm_info[id]['color'], lw=2, marker=algorithm_info[id]['marker'])
+            else: plt.plot(dataX, dataY, label=algorithm_info[id]['label'], color=algorithm_info[id]['color'], lw=2, marker=algorithm_info[id]['marker'])
+        plt.legend(loc=loc)
+        plt.xlabel(getLatexForString('\# of documents (10^4)')); plt.ylabel(getLatexForString('Running time (s)')); plt.title(getLatexForString(title))
+        locs,labels = plt.xticks()
+        plt.xticks(locs, map(lambda x: "%d"%(x/10000), locs))
+        if xmax: plt.xlim(xmax=xmax) 
+        if xmin: plt.xlim(xmin=xmin) 
+        plt.savefig(fileName)
+        
+    @staticmethod
+    def quality(quality_type, *iterators, **kwargs):
+        if quality_type=='f1': print kwargs.get('f1_id')
+        else: print quality_type
         for id, iterator in iterators:
             dataY = []
-            for data in iterator: dataY.append(data[quality_type])
-            print id, np.mean(dataY)
-
+            for data in iterator: 
+                if quality_type=='f1':
+                    f1_type = None
+                    if kwargs.get('f1_id')=='f1': f1_type=0
+                    elif kwargs.get('f1_id')=='precision': f1_type=1
+                    elif kwargs.get('f1_id')=='recall': f1_type=2
+                    if type(data[quality_type])==type([]): dataY.append(data[quality_type][f1_type])
+                    else: dataY.append(0)
+                else: dataY.append(data[quality_type])
+            print id, '%0.2f'%np.mean(dataY[2:])
+            
 if __name__ == '__main__':
-
 #    CompareAlgorithms.runningTimes(
 #                                   ('kmeans', DataIterators.kmeans()), 
 #                                   ('kmeans_mr', DataIterators.kmeansmr()), 
 #                                   ('cda_unopt', DataIterators.unoptimized()),
-#                                   loc=2, file_name='running_time_kmeans.eps', xmin=800, xmax=95000,
+#                                   loc=2, file_name='running_time_kmeans.pdf', xmin=800, xmax=97000,
 #                                   title='Running time comparison of Streaming-CDA with k-Means'
 #                                )
     
@@ -103,22 +128,21 @@ if __name__ == '__main__':
 #                                   ('cda_it', DataIterators.cdait()), 
 #                                   ('cda_mr', DataIterators.cdamr()), 
 #                                   ('cda_unopt', DataIterators.unoptimized()),
-#                                   loc=2, file_name='running_time_cda.eps', xmin=800, xmax=550000,
+#                                   loc=2, file_name='running_time_cda.pdf', xmin=800, xmax=550000,
 #                                   title='Running time comparison of Streaming-CDA with other CDA'
 #                                )
 
-    CompareAlgorithms.runningTimes(
-                                   ('cda', DataIterators.optimized()), 
-                                   ('cda_unopt', DataIterators.unoptimized()),
-                                   loc=2, file_name='running_time_opt_unopt_cda.eps', xmin=800, log=False,
-                                   title='Running time comparison between optimized and un-optimized CDA'
-                                )
+#    CompareAlgorithms.runningTimesWithCDA(
+#                                   ('cda', DataIterators.optimized()), 
+#                                   ('cda_unopt', DataIterators.unoptimized()),
+#                                   loc=2, file_name='running_time_opt_unopt_cda.pdf', xmin=55000, xmax=10**6, log=True,
+#                                   title='Running time performance after parameters estimation.'
+#                                )
     
-#    CompareAlgorithms.runningTimes(('cda_it', DataIterators.cdait()), ('cda', DataIterators.optimized()), ('cda_mr', DataIterators.cdamr()), ('cda_unopt', DataIterators.unoptimized()))
-#    CompareAlgorithms.runningTimes(('cda', DataIterators.optimized()), ('cda_unopt', DataIterators.unoptimized()))
+    CompareAlgorithms.quality('purity', ('kmeans', DataIterators.kmeans()), ('cda_it', DataIterators.cdait()), ('cda_unopt', DataIterators.unoptimized()), ('cda', DataIterators.optimized()))
+    CompareAlgorithms.quality('nmi', ('kmeans', DataIterators.kmeans()), ('cda_it', DataIterators.cdait()), ('cda_unopt', DataIterators.unoptimized()), ('cda', DataIterators.optimized()))
+    CompareAlgorithms.quality('f1', ('kmeans', DataIterators.kmeans()), ('cda_it', DataIterators.cdait()), ('cda_unopt', DataIterators.unoptimized()), ('cda', DataIterators.optimized()), f1_id='f1')
+    CompareAlgorithms.quality('f1', ('kmeans', DataIterators.kmeans()), ('cda_it', DataIterators.cdait()), ('cda_unopt', DataIterators.unoptimized()), ('cda', DataIterators.optimized()), f1_id='precision')
+    CompareAlgorithms.quality('f1', ('kmeans', DataIterators.kmeans()), ('cda_it', DataIterators.cdait()), ('cda_unopt', DataIterators.unoptimized()), ('cda', DataIterators.optimized()), f1_id='recall')
+    
 
-#    CompareAlgorithms.quality('purity', ('cda', DataIterators.optimized()), ('cda_unopt', DataIterators.unoptimized()))
-#    CompareAlgorithms.quality('nmi', ('cda', DataIterators.optimized()), ('cda_unopt', DataIterators.unoptimized()))
-#    for d in DataIterators.cdamr(): 
-#        del d['clusters']
-#        print d
