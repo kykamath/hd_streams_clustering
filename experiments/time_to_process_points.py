c7a1b1720e263255363475588611979ba1402244
Krishna Kamath <krishna.kamath@gmail.com>
2011-09-30 17:45:29 -0500

    modified experiments

--- experiments/time_to_process_points.py
+++ experiments/time_to_process_points.py
@@ -4,12 +4,13 @@ Created on Sep 30, 2011
 @author: kykamath
 '''
 from library.twitter import TweetFiles
+from library.file_io import FileIO
 time_to_process_points = '/mnt/chevron/kykamath/data/twitter/lsh_clustering/time_to_process_points/'
 
 def generateData():
     i = 0
     for line in TweetFiles.iterateTweetsFromGzip('/mnt/chevron/kykamath/data/twitter/lsh_clustering/clustering_quality_experts_folder/data/1000000.gz'):
-        print line
+        FileIO.writeToFileAsJson(line, time_to_process_points+'%s'%(i%50000))
         i+=1
         if i==10: break
 
