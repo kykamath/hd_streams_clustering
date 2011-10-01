c7b9aadaac7705f693e79bbe50b9242df8375d50
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-01 16:12:23 -0500

    modified hd delayed

--- hd_streams_clustering.py
+++ hd_streams_clustering.py
@@ -185,11 +185,14 @@ class HDSkipStreamClustering(StreamingLSHClustering):
                     distance = Vector.euclideanDistance(streamObject, previousStreamObject)
                     print i, len(self.clusters), distance
 #                    if distance>10: self.getClusterAndUpdateExistingClusters(self.streamIdToStreamObjectMap[message.streamId])
-                self.updateDimensionsMethod.call(message.timeStamp, hdStreamClusteringObject=self, currentMessageTime=message.timeStamp)
-                self.clusterFilteringMethod.call(message.timeStamp, hdStreamClusteringObject=self, currentMessageTime=message.timeStamp)
+
+#                self.updateDimensionsMethod.call(message.timeStamp, hdStreamClusteringObject=self, currentMessageTime=message.timeStamp)
+#                self.clusterFilteringMethod.call(message.timeStamp, hdStreamClusteringObject=self, currentMessageTime=message.timeStamp)
+
 #                self.clusterAnalysisMethod.call(message.timeStamp, hdStreamClusteringObject=self, currentMessageTime=message.timeStamp)
-                self.clusterAnalysisMethod.call(message.timeStamp, hdStreamClusteringObject=self, currentMessageTime=message.timeStamp, numberOfMessages=i)
-                print i, len(self.clusters)
+
+#                self.clusterAnalysisMethod.call(message.timeStamp, hdStreamClusteringObject=self, currentMessageTime=message.timeStamp, numberOfMessages=i)
+#                print i, len(self.clusters)
                 i+=1
 #                self.getClusterAndUpdateExistingClusters(streamObject)
 #            self.getClusterAndUpdateExistingClusters(message)
