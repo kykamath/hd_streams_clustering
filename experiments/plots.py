800fb0e0f002dce55ff302f0ef982ee49c26d0a0
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-02 13:51:07 -0500

    modified experiments

--- experiments/plots.py
+++ experiments/plots.py
@@ -166,21 +166,21 @@ class CompareAlgorithms:
         plt.savefig('crowds_quality.pdf')
         
 if __name__ == '__main__':
-#    CompareAlgorithms.runningTimes(
-#                                   ('kmeans', DataIterators.kmeans()), 
-#                                   ('kmeans_mr', DataIterators.kmeansmr()), 
-#                                   ('cda_unopt', DataIterators.unoptimized()),
-#                                   loc=2, file_name='running_time_kmeans', xmin=800, xmax=1100000,
-#                                   title='Running time comparison of Stream-CDA with k-means'
-#                                )
-    
     CompareAlgorithms.runningTimes(
-                                   ('cda_it', DataIterators.cdait()), 
-                                   ('cda_mr', DataIterators.cdamr()), 
+                                   ('kmeans', DataIterators.kmeans()), 
+                                   ('kmeans_mr', DataIterators.kmeansmr()), 
                                    ('cda_unopt', DataIterators.unoptimized()),
-                                   loc=2, file_name='running_time_cda', xmin=800, xmax=1100000,
-                                   title='Running time comparison of Stream-CDA with other CDA'
+                                   loc=2, file_name='running_time_kmeans', xmin=800, xmax=1100000,
+                                   title='Running time comparison of Stream-CDA with k-means'
                                 )
+    
+#    CompareAlgorithms.runningTimes(
+#                                   ('cda_it', DataIterators.cdait()), 
+#                                   ('cda_mr', DataIterators.cdamr()), 
+#                                   ('cda_unopt', DataIterators.unoptimized()),
+#                                   loc=2, file_name='running_time_cda', xmin=800, xmax=1100000,
+#                                   title='Running time comparison of Stream-CDA with other CDA'
+#                                )
 
 #    CompareAlgorithms.runningTimesWithCDA(
 #                                   ('cda', DataIterators.optimized()), 
